bc472fed2b08e11da3c66eae5b12de13b1026498
MandieBot <amandabotinelly@gmail.com>
2022-11-01 17:09:38 -0600

    Setting up files for modification

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+//Array of objects
+
+var questions = [
+  {
+    title: "Where do wild turkeys sleep?",
+    choices: ["Underground", "In cages", "In trees"],
+    answer: "In trees",
+  },
+  {
+    title: "Which Founding Father preferred turkeys to bald eagles?",
+    choices: ["Benjamin Franklin", "John Adams", "Thomas Jefferson"],
+    answer: "Benjamin Franklin",
+  },
+  {
+    title: "How fast can wild turkeys fly?",
+    choices: ["15mph", "30mph", "55mph"],
+    answer: "55mph",
+  },
+];
